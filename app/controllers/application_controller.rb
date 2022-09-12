d864bfe260860265e4931f4c8095cb7f3e0a8277
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-09-12 15:35:58 +0300

    created rb files with code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,114 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+
+  # Book routes
+  get "/books" do 
+    books = Book.all
+    books.to_json(include: :author)
   end
+
+  get "/books/:id" do 
+    books = Book.find(params[:id])
+    books.to_json
+  end
+
+
+  # get "/books/all/title_ascending" do 
+  #   books = Book.all.title_ascending
+  #   books.to_json
+  # end
+  # get "/books/all/title_descending" do 
+  #   books = Book.all.title_descending
+  #   books.to_json
+  # end
+  # get "/books/all/by_price_ascending" do
+  #   books = Book.all.by_price_ascending
+  #   books.to_json
+  # end
+  # get "/books/all/by_price_descending" do
+  #   books = Book.all.by_price_descending
+  #   books.to_json
+  # end
+
+
+  post "/books" do 
+    # use author name to see if that author exists
+    # if yes, create that book in the authors collection
+    # if no, create an author and then create that book in that authors collection
+    # active record associations
+    author = Author.find_or_create_by(name: params[:author_name])
+
+    new_book = author.books.create(
+      title: params[:title],
+      description: params[:description],
+      price: params[:price],
+      pages: params[:pages],
+    )
+    new_book.to_json
+  end
+
+  patch "/books/:id" do
+    book = Book.find(params[:id])
+    book.update(
+      title: params[:title],
+      description: params[:description],
+      price: params[:price],
+      pages: params[:pages],
+    )
+    
+    author = book.author
+    author.update(
+      name: params[:author_name]
+    )
+    author.to_json
+    book.to_json
+  end
+
+  delete "/books/:id" do
+    books = Book.find(params[:id])
+    books.destroy
+    books.to_json
+  end
+
+
+
+
+
+  # Author routes
+  get "/authors" do 
+    authors = Author.all
+    authors.to_json(include: :books)
+  end
+
+  get "/authors/:id" do 
+    authors = Author.find(params[:id])
+    authors.to_json
+  end
+
+
+  post "/authors" do 
+    new_author = Author.create(
+      name: params[:name]
+    )
+    new_author.to_json
+  end
+
+  patch "/authors/:id" do
+    author = Author.find(params[:id])
+    author.update(
+      name: params[:name],
+      )
+      author.to_json
+      binding.pry
+  end
+
+  # delete "/authors/:id" do
+  #   authors = Author.find(params[:id])
+  #   authors.destroy
+  #   authors.to_json
+  # end
 
 end
